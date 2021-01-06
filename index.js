2bcc6afada9c1d8b579efc41236833b6689a674a
Michael Zinoviev <mishazinoviev91@gmail.com>
2021-01-06 14:48:49 +0200

    Added collapse of more info.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const usersUrl = "https://api.coingecko.com/api/v3/coins/";
-const result = $.ajax({
-  method: "GET",
-  url: usersUrl,
-});
+home();
+function clearActiveClassesAndMain() {
+  $(".home").removeClass("active");
+  $(".about").removeClass("active");
+  $(".live-reports").removeClass("active");
+  $(".main>*").remove();
+}
+function home() {
+  clearActiveClassesAndMain();
+  $(".home").addClass("active");
+  const usersUrl = "https://api.coingecko.com/api/v3/coins/";
+  const result = $.ajax({
+    method: "GET",
+    url: usersUrl,
+  });
 
-result.always((res) => {
-  for (let i = 0; i < 100; i++) {
-    $(".main").append(
-      `<div class="card m-3">
+  result.always((res) => {
+    for (let i = 0; i < 100; i++) {
+      $(".main").append(
+        `<div class="card m-3">
             <div class="card-body">
             <div class="card-upper">
-                <h5 class="card-title">${res[i].symbol}</h5>
+                <h5 class="card-title">${res[i].symbol} <img src="${res[i].image.thumb}" alt="coin-image" /></h5>
                 <div class="form-check form-switch">
                 <input
                     class="form-check-input"
@@ -19,15 +29,44 @@ result.always((res) => {
                 />
                 </div>
             </div>
-            
-            <p class="card-text">${res[i].id}</p>
+
+            <p class="card-text">${res[i].name} </p>
             <div class="card-bottom">
-            <button type="button" class="btn btn-primary">More Info</button>
-            <img src="${res[i].image.small}" alt="coin-image"/>
+                <button
+                data-bs-toggle="collapse"
+                data-bs-target="#more-info-${res[i].id}"
+                type="button"
+                class="btn btn-primary"
+                >
+                More Info
+                </button>
+
+                <!-- <img src="${res[i].image.small}" alt="coin-image" /> -->
+            </div>
+            <div id="more-info-${res[i].id}" class="collapse row">
+                <div class="col-6">
+                    <p ><b>USD:</b> ${res[i].market_data.current_price.usd} <b>$</b></p>
+                    <p ><b>EUR:</b> ${res[i].market_data.current_price.eur} <b>€</b></p>
+                    <p ><b>ILS:</b> ${res[i].market_data.current_price.ils} <b>₪</b></p>
+                </div>
+                <img class="col-6" src="${res[i].image.small}" alt="coin-image" />
             </div>
             </div>
         </div>`
-    );
-  }
-});
-result.fail((err) => console.log(err));
+      );
+    }
+  });
+  result.fail((err) => console.log(err));
+}
+
+function liveReports() {
+  clearActiveClassesAndMain();
+  $(".live-reports").addClass("active");
+  $(".main").append(`<h1>liveReports</h1>`);
+}
+
+function about() {
+  clearActiveClassesAndMain();
+  $(".about").addClass("active");
+  $(".main").append(`<h1>About</h1>`);
+}
