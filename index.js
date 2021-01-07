10cdc7e6b2771aad1a1634dfecaa7d1f3d7882df
Michael Zinoviev <mishazinoviev91@gmail.com>
2021-01-07 11:40:07 +0200

    Added about section

--- index.js
+++ index.js
@@ -40,8 +40,6 @@ function home() {
                 >
                 More Info
                 </button>
-
-                <!-- <img src="${res[i].image.small}" alt="coin-image" /> -->
             </div>
             <div id="more-info-${res[i].id}" class="collapse row">
                 <div class="col-6">
@@ -68,5 +66,50 @@ function liveReports() {
 function about() {
   clearActiveClassesAndMain();
   $(".about").addClass("active");
-  $(".main").append(`<h1>About</h1>`);
+  $(".main").append(
+    `
+    <div class="d-flex justify-content-center flex-column">
+    <div class="d-flex justify-content-between">
+      <p class="masthead-subheading shadow-lg p-3 mb-5 rounded  m-4">
+        <b>About Cryptonite</b><br/><br/> Cryptonite is the world's most-referenced
+        price-tracking website for cryptoassets in the rapidly growing
+        cryptocurrency space. Its mission is to make crypto discoverable
+        and efficient globally by empowering retail users with unbiased,
+        high quality and accurate information for drawing their own
+        informed conclusions.<br/><br/>
+         Founded by Michael Zinoviev in January 2020,
+        Cryptonite has quickly grown to become the most trusted source
+        by users, institutions, and media for comparing thousands of
+        cryptoassets and is commonly cited by CNBC, Bloomberg, and other
+        major news outlets. (Even the U.S. government uses Cryptonite's
+        data for research and reports!)
+      </p>
+      <div class="divider-custom-icon m-5 d-flex align-items-center">
+        <i class="fab fa-bitcoin fa-7x"></i>
+      </div>
+    </div>
+    <div
+      class="d-flex justify-content-center flex-row align-items-center mt-5"
+    >
+      <img
+        class="profile-picture m-1"
+        src="./images/circle-cropped.png"
+        alt="profile picture"
+      />
+
+      <div class="d-flex justify-content-center flex-column m-5">
+        <a href="https://www.facebook.com/mishazinoviev/"
+          ><i class="fab fa-fw fa-facebook-f fa-2x"></i></a
+        ><a href="https://twitter.com/mishazino"
+          ><i class="fab fa-fw fa-twitter fa-2x"></i></a
+        ><a href="https://www.linkedin.com/in/michaelzinoviev-webdev/"
+          ><i class="fab fa-fw fa-linkedin-in fa-2x"></i></a
+        ><a href="https://github.com/MishkaZi"
+          ><i class="fab fa-fw fa-github fa-2x"></i
+        ></a>
+      </div>
+    </div>
+  </div>
+    `
+  );
 }
