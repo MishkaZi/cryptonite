e6909c831e0d9624d16a49df808fa0095dd5c1f6
Michael Zinoviev <mishazinoviev91@gmail.com>
2021-01-23 19:33:05 +0200

    Fixed bug preventing from live-reports to load.

--- JS/live-report.js
+++ JS/live-report.js
@@ -15,67 +15,69 @@ const liveReports = async () => {
             <div id="chartContainer" style="height: 600px; width: 100%;"></div>
       
         `);
-    }
+        
+      var options = {
+        exportEnabled: true,
+        animationEnabled: true,
+        title: { text: "Cryptocoins" },
+        subtitles: [{ text: "in USD" }],
+        axisX: { title: "Updates every 2 seconds" },
+        axisY: {
+          title: "Coin Value",
+        },
+        toolTip: { shared: true },
+        legend: {
+          cursor: "pointer",
+          verticalAlign: "top",
+          fontSize: 22,
+          fontColor: "dimGrey",
+          itemclick: toggleDataSeries,
+        },
+        data: [],
+      };
+      var chart = new CanvasJS.Chart("chartContainer", options);
 
-    var options = {
-      exportEnabled: true,
-      animationEnabled: true,
-      title: { text: "Cryptocoins" },
-      subtitles: [{ text: "in USD" }],
-      axisX: { title: "Updates every 2 seconds" },
-      axisY: {
-        title: "Coin Value",
-      },
-      toolTip: { shared: true },
-      legend: {
-        cursor: "pointer",
-        verticalAlign: "top",
-        fontSize: 22,
-        fontColor: "dimGrey",
-        itemclick: toggleDataSeries,
-      },
-      data: [],
-    };
-    var chart = new CanvasJS.Chart("chartContainer", options);
-
-    for (let i = 0; i < toggledCoins.length; i++) {
-      options.data.push({
-        type: "spline",
-        name: `${toggledCoins[i].name}`,
-        xValueFormatString: "hh:mm:ss",
-        showInLegend: true,
-        dataPoints: [],
-      });
-    }
-    showLoader();
-    const updateChart = async () => {
-      const coinsPrices = await getCoinsPrices();
-      let time = new Date().toLocaleTimeString();
       for (let i = 0; i < toggledCoins.length; i++) {
-        let coin = options.data[i].dataPoints;
-
-        coin.push({
-          y: coinsPrices[Object.keys(coinsPrices)[i]].USD,
-          label: time,
+        options.data.push({
+          type: "spline",
+          name: `${toggledCoins[i].name}`,
+          xValueFormatString: "hh:mm:ss",
+          showInLegend: true,
+          dataPoints: [],
         });
-  
       }
-      removeLoader();
-    
+      showLoader();
+      const updateChart = async () => {
+        const coinsPrices = await getCoinsPrices();
+        let time = new Date().toLocaleTimeString();
+        for (let i = 0; i < toggledCoins.length; i++) {
+          let coin = options.data[i].dataPoints;
+
+          coin.push({
+            y: coinsPrices[Object.keys(coinsPrices)[i]].USD,
+            label: time,
+          });
+        }
+        removeLoader();
+
+        chart.render();
+      };
+
+      function toggleDataSeries(e) {
+        if (
+          typeof e.dataSeries.visible === "undefined" ||
+          e.dataSeries.visible
+        ) {
+          e.dataSeries.visible = false;
+        } else {
+          e.dataSeries.visible = true;
+        }
+        e.chart.render();
+      }
+
       chart.render();
-    };
-
-    function toggleDataSeries(e) {
-      if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
-        e.dataSeries.visible = false;
-      } else {
-        e.dataSeries.visible = true;
-      }
-      e.chart.render();
+      myInterval = setInterval(() => updateChart(), 2000);
     }
-
-    chart.render();
-    myInterval = setInterval(() => updateChart(), 2000);
   } catch (error) {
     console.log(error);
   }
